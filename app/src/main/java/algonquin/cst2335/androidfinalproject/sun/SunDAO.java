a672a16a9e8b0d9f432f4cdac5a614b5ca69000e
qdsongyu <qdsongyu@hotmail.com>
2023-11-16 19:51:13 -0500

    Sun.java & SunDAO.java initial commit

---
+++ app/src/main/java/algonquin/cst2335/androidfinalproject/sun/SunDAO.java
@@ -0,0 +1,20 @@
+package algonquin.cst2335.androidfinalproject.sun;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+public interface SunDAO {
+    @Insert
+    long insertSun(Sun s);
+
+    @Query("Select * from Sun")
+    List<Sun> getAllSuns();
+
+    @Delete
+    void deleteSun(Sun s);
+
+}
