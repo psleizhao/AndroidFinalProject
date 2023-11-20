6283765fe6a7e35c79f2f38cdebfba75d1412cfc
Lei Zhao <zhao0261@algonquinlive.com>
2023-11-20 13:28:57 -0500

    Added button onClickListener to get to the source url for details

--- app/src/main/java/algonquin/cst2335/androidfinalproject/recipe/RecipeDetailsFragment.java
+++ app/src/main/java/algonquin/cst2335/androidfinalproject/recipe/RecipeDetailsFragment.java
@@ -1,13 +1,20 @@
 package algonquin.cst2335.androidfinalproject.recipe;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 import java.io.File;
@@ -20,6 +27,7 @@ public class RecipeDetailsFragment extends Fragment {
 
     public RecipeDetailsFragment(Recipe r) {selected = r;}
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
@@ -32,10 +40,32 @@ public class RecipeDetailsFragment extends Fragment {
             binding.recipeImageView.setImageBitmap(img);
         }
         binding.recipeNameText.setText(selected.recipeName);
-        binding.summary.setText(selected.summary);
-        binding.sourceButton.setOnClickListener(clk -> {
-                    binding.sourceButton.setText("Went to source");
-                }
+
+//        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            Spanned spannedText = Html.fromHtml(selected.summary, Html.FROM_HTML_MODE_LEGACY);
+            binding.summary.setText(spannedText);
+
+//        }
+
+//        binding.summary.setText(spannedText);
+
+        //   binding.sourceButton.setText("Went to source");
+        binding.sourceButton.setOnClickListener(v -> {
+       // Get the URL associated with the button
+       String url = selected.getSrcUrl(); // Replace with your actual URL
+
+       // Create an Intent to open the URL in a web browser
+       Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+
+       // Check if there's an app to handle the Intent
+       if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+           // Open the URL in the web browser
+           startActivity(intent);
+       } else {
+           // Handle the case where there is no app to handle the Intent
+           Toast.makeText(getContext(), "No app to handle URL", Toast.LENGTH_SHORT).show();
+       }
+   }
         );
         return binding.getRoot();
     }
