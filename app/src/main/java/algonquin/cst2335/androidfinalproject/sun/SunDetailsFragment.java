08e5f7af4a80b8a50df5b487c62b7e696a807716
qdsongyu <qdsongyu@hotmail.com>
2023-11-29 00:33:34 -0500

    SunDAO updated the "void updateSun"

--- app/src/main/java/algonquin/cst2335/androidfinalproject/sun/SunDetailsFragment.java
+++ app/src/main/java/algonquin/cst2335/androidfinalproject/sun/SunDetailsFragment.java
@@ -31,6 +31,7 @@ public class SunDetailsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
+        setRetainInstance(true);
         //inflate an XML layout for this Fragment
         SunDetailsLayoutBinding binding = SunDetailsLayoutBinding.inflate(getLayoutInflater());
 
