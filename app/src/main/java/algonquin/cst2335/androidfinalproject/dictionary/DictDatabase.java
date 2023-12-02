a2a7d200cefb7f543327345fce72580dc431c9b6
Yuling Guo <guo00125@algonquinlive.com>
2023-12-02 12:21:04 -0500

    modified

--- app/src/main/java/algonquin/cst2335/androidfinalproject/dictionary/DictDatabase.java
+++ app/src/main/java/algonquin/cst2335/androidfinalproject/dictionary/DictDatabase.java
@@ -2,18 +2,11 @@ package algonquin.cst2335.androidfinalproject.dictionary;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
+
 /**
  * The {@code DictDatabase} class represents the Room database for the dictionary feature
  * in the Android final project. It defines the database configuration, including the
  * entities and version information. This class extends {@link RoomDatabase}.
- *
- * <p>Usage example:</p>
- * <pre>
- * {@code
- * DictDatabase db = Room.databaseBuilder(context, DictDatabase.class, "dictdb").build();
- * DictDAO dictDAO = db.DictDAO();
- * }
- * </pre>
  *
  * @author Yuling Guo
  * @version 1.0
